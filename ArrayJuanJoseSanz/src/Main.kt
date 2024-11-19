d3527a94eca5b6498b9e17779e38849612d0dc71
CrazuSmile <juanjosesanzlopez@gmail.com>
2024-11-19 12:16:31 +0100

    Ejercicio 4 Arrays parcialmente finalizdo

---
+++ ArrayJuanJoseSanz/src/Main.kt
@@ -0,0 +1,51 @@
+/*
+ * Progama de matrices del ejercicio 5
+ * @author Juan Jose Sanz
+ * @version 1
+ */
+
+fun main() {
+    var array: Array<Int> = arrayOf()
+    do {
+        array += inputArray(array)
+
+        println("Numeros pares del Array: ")
+        pair(array)
+
+        print(
+            ("""|¿Quieres terminar el programa?
+        | 1. Si.
+        | 2. No.
+        |Respuesta: """.trimMargin())
+        )
+        val respond = readln().toInt()
+
+    } while (respond == 2)
+
+    println("Adios")
+}
+
+fun inputArray(array: Array<Int>): Array<Int> {
+    var newArray = array
+    while (true) {
+        println("Introduzca un numero (introduzca 'fin' para finalizar el Array)")
+        val input = readln()
+
+        if (input == "fin") {
+            break
+        }
+
+        val number = input.toInt()
+        newArray += number
+
+    }
+    return newArray
+}
+
+fun pair(array: Array<Int>) {
+    for (i in array.indices) {
+        if (array[i] % 2f == 0f) {
+            println("${array[i]}")
+        }
+    }
+}
