e2434b03f692ced6ff9caed8f7dbae7d7d301280
CrazuSmile <juanjosesanzlopez@gmail.com>
2025-01-22 18:30:11 +0100

    Arreglo de la lista

--- DragonBallJuanJoseSanz/app/src/main/java/com/juanjosesanz/dragonballjuanjosesanz/ui/screens/DragonBall.kt
+++ DragonBallJuanJoseSanz/app/src/main/java/com/juanjosesanz/dragonballjuanjosesanz/ui/screens/DragonBall.kt
@@ -1,5 +1,7 @@
 package com.juanjosesanz.dragonballjuanjosesanz.ui.screens
 
+import android.util.Log
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -9,10 +11,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -29,10 +33,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.juanjosesanz.dragonballjuanjosesanz.R
+import com.juanjosesanz.dragonballjuanjosesanz.model.DragonBallCharacter
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun DragonBall() {
     val orange = Color(0xFFF89F02)
@@ -41,6 +48,7 @@ fun DragonBall() {
 
 
     var boxState by rememberSaveable { mutableStateOf(false) }
+    val dbChar = DragonBallCharacter
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -84,9 +92,40 @@ fun DragonBall() {
                 .padding(innerPadding)
         ) {
             Box() {
-                LazyColumn {
+                Row {
+                    LazyColumn {
+                        val groupedCharacter: Map<Char, List<DragonBallCharacter>> =
+                            dbChar.sorted().groupBy { it.spanishName.first() }
+                        Log.i("--->", groupedCharacter.toString())
+                        groupedCharacter.forEach { header, group ->
+                            stickyHeader {
+                                Text(
+                                    text = "$header",
+                                    textAlign = TextAlign.Center,
+                                    fontWeight = FontWeight.Bold,
+                                    modifier = Modifier
+                                        .background(Color.Yellow)
+                                        .fillParentMaxSize()
+                                )
+                                HorizontalDivider(
+                                    color = Color.Black,
+                                    thickness = 1.dp
+                                )
+                            }
+                            items(group) { character ->
+                                Text(
+                                    text = character.spanishName,
+                                    fontWeight = FontWeight.Bold,
+                                    modifier = Modifier
+                                        .padding(start = 40.dp)
+                                        .padding(vertical = 8.dp)
+                                )
+                            }
 
+                        }
+                    }
                 }
+
                 if (boxState) {
                     Box(
                         modifier = Modifier.fillMaxSize(),
